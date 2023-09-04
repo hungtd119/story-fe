c864505da4bb5c44b37452f388005929fa38898b
hungit119 <tranduyhungdz119@gmail.com>
2023-09-04 21:57:27 +0700

    xây dựng page băng canvas

--- src/app/components/story-play/story-play.component.ts
+++ src/app/components/story-play/story-play.component.ts
@@ -31,7 +31,20 @@ export class StoryPlayComponent implements OnInit, AfterViewInit {
   myCanvasRef!: ElementRef<HTMLCanvasElement>;
   @ViewChild('secondCanvas', { static: false })
   secondCanvasRef!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('textCanvas', { static: false })
+  textCanvasRef!: ElementRef<HTMLCanvasElement>;
   page$: Observable<Page> = this.store.select(selectPage);
+
+  syncText = [
+    { s: 0, e: 1200, w: 'Hey,' },
+    { s: 1200, e: 1490, w: 'do' },
+    { s: 1490, e: 1700, w: 'you' },
+    { s: 1700, e: 2100, w: 'want' },
+    { s: 2100, e: 2260, w: 'to' },
+    { s: 2260, e: 3580, w: 'eat' },
+    { s: 3580, e: 3880, w: 'salad' },
+    { s: 3880, e: 3880, w: '' },
+  ];
 
   interactions: any[] = [
     {
@@ -60,6 +73,8 @@ export class StoryPlayComponent implements OnInit, AfterViewInit {
     },
   ];
 
+  currentIndex: number = 0;
+
   constructor(
     private route: ActivatedRoute,
     private store: Store<PageState>,
@@ -80,20 +95,30 @@ export class StoryPlayComponent implements OnInit, AfterViewInit {
 
         const canvasCom: HTMLCanvasElement = this.secondCanvasRef.nativeElement;
         const ctxCom = canvasCom.getContext('2d');
-        if (ctx && ctxCom) {
+
+        const canvasText: HTMLCanvasElement = this.textCanvasRef.nativeElement;
+        const ctxText = canvasCom.getContext('2d');
+        if (ctx && ctxCom && ctxText) {
           ctx.clearRect(0, 0, canvas.width, canvas.height);
           const img = new Image();
           img.src = pg.image?.path;
           img.onload = () => {
             ctx.drawImage(img, 0, 80, canvas.width, canvas.height - 100);
-            ctx.font = '48px serif';
-            ctx.textAlign = 'center';
-            ctx.textBaseline = 'middle';
-            ctx.fillText(
-              pg.texts[0].text,
-              canvas.width / 2,
-              canvas.height / 10
+            this.drawText(
+              ctxText,
+              canvasText.width / 4,
+              canvasText.height / 10,
+              '48px serif',
+              'black'
             );
+            setTimeout(() => {
+              this.hightLightText(
+                ctxText,
+                this.textCanvasRef,
+                canvasText.width / 4,
+                canvasText.height / 10
+              );
+            }, 1000);
             pg.interactions.forEach((interaction) => {
               interaction.positions.forEach((position: any) => {
                 this.renderer.listen(
@@ -112,62 +137,7 @@ export class StoryPlayComponent implements OnInit, AfterViewInit {
                         y >= position.position_y &&
                         y <= position.position_y + position.height
                       ) {
-                        const fontSize = 18;
-                        const borderRadius = 6;
-
-                        ctxCom.font = fontSize + 'px Arial';
-                        const textWidth = ctxCom.measureText(
-                          interaction.text.text
-                        ).width;
-
-                        const padding = 6;
-                        const rectWidth = textWidth + 2 * padding;
-                        const rectHeight = fontSize + 2 * padding + 4;
-
-                        ctxCom.beginPath();
-                        ctxCom.moveTo(x + borderRadius, y);
-                        ctxCom.lineTo(x + rectWidth - borderRadius, y);
-                        ctxCom.arcTo(
-                          x + rectWidth,
-                          y,
-                          x + rectWidth,
-                          y + borderRadius,
-                          borderRadius
-                        );
-                        ctxCom.lineTo(
-                          x + rectWidth,
-                          y + rectHeight - borderRadius
-                        );
-                        ctxCom.arcTo(
-                          x + rectWidth,
-                          y + rectHeight,
-                          x + rectWidth - borderRadius,
-                          y + rectHeight,
-                          borderRadius
-                        );
-                        ctxCom.lineTo(x + borderRadius, y + rectHeight);
-                        ctxCom.arcTo(
-                          x,
-                          y + rectHeight,
-                          x,
-                          y + rectHeight - borderRadius,
-                          borderRadius
-                        );
-                        ctxCom.lineTo(x, y + borderRadius);
-                        ctxCom.arcTo(x, y, x + borderRadius, y, borderRadius);
-
-                        ctxCom.fillStyle = 'black';
-                        ctxCom.fill();
-
-                        ctxCom.fillStyle = 'white';
-                        ctxCom.fillText(
-                          interaction.text.text,
-                          x + padding,
-                          y + padding + fontSize
-                        );
-                        setTimeout(() => {
-                          ctxCom.clearRect(x, y, rectWidth + 1, rectHeight);
-                        }, 1000);
+                        console.log(interaction);
                       }
                     });
                   }
@@ -181,5 +151,93 @@ export class StoryPlayComponent implements OnInit, AfterViewInit {
         console.log(error);
       },
     });
+  }
+  drawText(
+    ctx: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    font: string,
+    color: string
+  ) {
+    const fontSize = 16;
+    const padding = 10;
+    ctx.font = font;
+    ctx.textBaseline = 'middle';
+    let currentX = x;
+    for (let index = 0; index < this.syncText.length; index++) {
+      const word = this.syncText[index].w;
+      ctx.fillStyle = color;
+      ctx.fillText(word, currentX, y);
+      const wordWidth = ctx.measureText(word).width;
+      currentX += wordWidth + padding;
+    }
+  }
+  hightLightText(
+    ctx: CanvasRenderingContext2D,
+    canvasRef: ElementRef,
+    x: number,
+    y: number
+  ) {
+    this.currentIndex = 0;
+    this.animateHightLight(ctx, canvasRef, x, y);
+  }
+  animateHightLight(
+    ctx: CanvasRenderingContext2D,
+    canvasRef: ElementRef,
+    x: number,
+    y: number
+  ) {
+    if (this.currentIndex >= this.syncText.length) {
+      return;
+    }
+
+    const currentWord = this.syncText[this.currentIndex];
+
+    const startTime = Date.now();
+    const animate = () => {
+      const currentTime = Date.now();
+      const process =
+        (currentTime - startTime) / (currentWord.e - currentWord.s);
+      if (process >= 1) {
+        this.clearCanvas(ctx, canvasRef);
+        this.hightLightingText(currentWord, x, y, ctx);
+        this.currentIndex++;
+
+        if (this.currentIndex < this.syncText.length) {
+          this.animateHightLight(ctx, canvasRef, x, y);
+        }
+      } else {
+        this.clearCanvas(ctx, canvasRef);
+        this.hightLightingText(currentWord, x, y, ctx);
+        requestAnimationFrame(animate);
+      }
+    };
+    requestAnimationFrame(animate);
+  }
+  clearCanvas(ctx: CanvasRenderingContext2D, canvasRef: ElementRef) {
+    ctx.clearRect(
+      0,
+      0,
+      canvasRef.nativeElement.width,
+      canvasRef.nativeElement.height
+    );
+  }
+  hightLightingText(
+    currentWord: any,
+    x: number,
+    y: number,
+    ctx: CanvasRenderingContext2D
+  ) {
+    const padding = 10;
+    ctx.font = '48px serif';
+    let currentX = x;
+    for (let i = 0; i < this.syncText.length; i++) {
+      const word = this.syncText[i].w;
+      ctx.fillStyle = 'black';
+      if (this.syncText[i].w === currentWord.w) ctx.fillStyle = 'red';
+      ctx.fillText(word, currentX, y);
+      const wordWidth = ctx.measureText(word).width;
+      currentX += wordWidth + padding;
+    }
   }
 }
